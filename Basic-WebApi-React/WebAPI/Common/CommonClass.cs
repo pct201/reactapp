7e2bc3bc60040ed4a3e326591769551a3e1f8a87
Nishant Bharathan <nishant.bharathan@tatvasoft.com>
2020-02-21 16:21:48 +0530

    replace space with plus.

--- Basic-WebApi-React/WebAPI/Common/CommonClass.cs
+++ Basic-WebApi-React/WebAPI/Common/CommonClass.cs
@@ -43,13 +43,15 @@ namespace WebAPI.Common
                     clearText = Convert.ToBase64String(ms.ToArray());
                 }
             }
+
             return clearText;
         }
 
         public string Decrypt(string cipherText)
         {
+
             string EncryptionKey = configuration["EncryptionKey"];
-            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            byte[] cipherBytes = Convert.FromBase64String(cipherText.Replace(" ", "+"));
             using (Aes encryptor = Aes.Create())
             {
                 Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
