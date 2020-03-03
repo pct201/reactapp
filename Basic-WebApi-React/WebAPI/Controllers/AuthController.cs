8e08006ca246ca50abc68f3f3e4112433194e633
Nishant Bharathan <nishant.bharathan@tatvasoft.com>
2020-03-03 13:56:04 +0530

    remove additional code

--- Basic-WebApi-React/WebAPI/Controllers/AuthController.cs
+++ Basic-WebApi-React/WebAPI/Controllers/AuthController.cs
@@ -300,12 +300,4 @@ namespace WebAPI.Controllers
 
     }
 
-    public class TmpRefreshToken
-    {
-        public int UserId { get; set; }
-        public string Token { get; set; }
-        public string RefreshToken { get; set; }
-        public DateTime ExpireOn { get; set; }
-
-    }
 }
